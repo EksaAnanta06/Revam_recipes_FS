4d99372a53244923195e18447701e471de2de8d2
Eksa A <eksaananta@gmail.com>
2026-04-26 22:20:12 +0700

    t

--- Revamp_Recipes_FE/src/utils/constants.jsx
+++ Revamp_Recipes_FE/src/utils/constants.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://56d0-2402-5680-9319-7c84-2ee4-d77b-5d6-15ce.ngrok-free.app/';
+export const BASE_URL = 'https://56d0-2402-5680-9319-7c84-2ee4-d77b-5d6-15ce.ngrok-free.app';
 
 export const validateEmail = (email) => {
     return String(email)
