fbe7b82aba39255b34c27032f8a3cfe81477c5db
Eksa A <eksaananta@gmail.com>
2026-04-27 13:11:38 +0700

    y

--- Revamp_recipes_BE/index.js
+++ Revamp_recipes_BE/index.js
@@ -12,7 +12,7 @@ env.config();
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors({
-    origin: "https://56d0-2402-5680-9319-7c84-2ee4-d77b-5d6-15ce.ngrok-free.app", // Alamat frontend Vercel
+    origin: "https://revam-recipes-fs-98gi.vercel.app", // Alamat frontend Vercel
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "ngrok-skip-browser-warning"],
     credentials: true
